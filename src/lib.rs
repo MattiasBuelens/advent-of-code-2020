10c7bb1e983483b658f9eecdf64735fe5f86d796
Mattias Buelens <mattias@buelens.com>
2020-11-30 23:14:49 +0100

    Set up cargo-aoc
    Using my solution for 2019 day 1 as a quick test.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
-}
+extern crate aoc_runner;
+
+#[macro_use]
+extern crate aoc_runner_derive;
+
+pub mod day1;
+
+aoc_lib!{ year = 2019 }
