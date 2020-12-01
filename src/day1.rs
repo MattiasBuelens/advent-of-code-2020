536a31e6ae264c5656f256dbe22dd6438497b67d
Mattias Buelens <mattias@buelens.com>
2020-12-01 21:02:11 +0100

    Optimize day 1

--- src/day1.rs
+++ src/day1.rs
@@ -5,8 +5,8 @@ pub fn input_generator(input: &str) -> Vec<i32> {
 
 #[aoc(day1, part1)]
 pub fn part1(expenses: &[i32]) -> i32 {
-    for x in expenses {
-        for y in expenses {
+    for (i, &x) in expenses.iter().enumerate() {
+        for &y in expenses.iter().skip(i + 1) {
             if (x + y) == 2020 {
                 return x * y;
             }
@@ -17,9 +17,9 @@ pub fn part1(expenses: &[i32]) -> i32 {
 
 #[aoc(day1, part2)]
 pub fn part2(expenses: &[i32]) -> i32 {
-    for x in expenses {
-        for y in expenses {
-            for z in expenses {
+    for (i, &x) in expenses.iter().enumerate() {
+        for (j, &y) in expenses.iter().enumerate().skip(i + 1) {
+            for &z in expenses.iter().skip(j + 1) {
                 if (x + y + z) == 2020 {
                     return x * y * z;
                 }
