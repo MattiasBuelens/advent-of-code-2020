3489a5798cd11afc1bdd96e176eece5b22631c0c
Mattias Buelens <mattias@buelens.com>
2020-12-01 06:19:54 +0100

    Solve day 1

--- src/day1.rs
+++ src/day1.rs
@@ -1,14 +1,30 @@
 #[aoc_generator(day1)]
 pub fn input_generator(input: &str) -> Vec<i32> {
-    unimplemented!()
+    input.lines().map(|line| line.parse().unwrap()).collect()
 }
 
 #[aoc(day1, part1)]
-pub fn part1(input: &[i32]) -> i32 {
-    unimplemented!()
+pub fn part1(expenses: &[i32]) -> i32 {
+    for x in expenses {
+        for y in expenses {
+            if (x + y) == 2020 {
+                return x * y;
+            }
+        }
+    }
+    unreachable!()
 }
 
 #[aoc(day1, part2)]
-pub fn part2(input: &[i32]) -> i32 {
-    unimplemented!()
+pub fn part2(expenses: &[i32]) -> i32 {
+    for x in expenses {
+        for y in expenses {
+            for z in expenses {
+                if (x + y + z) == 2020 {
+                    return x * y * z;
+                }
+            }
+        }
+    }
+    unreachable!()
 }
