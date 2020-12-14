806a3dbba2e30f278d20d183375691f9160a40f7
Mattias Buelens <mattias@buelens.com>
2020-12-14 12:36:48 +0100

    Add template

---
+++ src/template.rs
@@ -0,0 +1,14 @@
+#[aoc_generator(day1)]
+pub fn input_generator(input: &str) -> Vec<i32> {
+    input.lines().map(|line| line.parse().unwrap()).collect()
+}
+
+#[aoc(day1, part1)]
+pub fn part1(input: &[i32]) -> i32 {
+    todo!()
+}
+
+#[aoc(day1, part2)]
+pub fn part2(input: &[i32]) -> i32 {
+    todo!()
+}
