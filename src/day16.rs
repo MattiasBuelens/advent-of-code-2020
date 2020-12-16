c9ab802cb2db3b32313716e6f11eacec2cea8101
Mattias Buelens <mattias@buelens.com>
2020-12-16 13:56:05 +0100

    Solve day 16 part 2

--- src/day16.rs
+++ src/day16.rs
@@ -1,6 +1,6 @@
 pub type Ticket = Vec<i32>;
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct Field {
     name: String,
     ranges: Vec<(i32, i32)>,
@@ -78,7 +78,48 @@ pub fn part1(input: &Input) -> i32 {
     invalid_values.sum()
 }
 
+fn assign_fields<'a>(fields: &[Field], tickets: &[Ticket]) -> Vec<(Field, usize)> {
+    let mut assigned = Vec::<(Field, usize)>::new();
+    let mut fields = fields.to_vec();
+    let mut indexes = (0..tickets[0].len()).collect::<Vec<usize>>();
+    assert_eq!(fields.len(), indexes.len());
+    'outer: while !fields.is_empty() {
+        for (i, field) in fields.iter().enumerate() {
+            let candidate_indexes = indexes
+                .iter()
+                .filter(|&&index| tickets.iter().all(|ticket| field.is_valid(ticket[index])))
+                .cloned()
+                .collect::<Vec<_>>();
+            if let [field_index] = candidate_indexes[..] {
+                // only one candidate index for this field, assign it
+                assigned.push((field.clone(), field_index));
+                fields.remove(i);
+                indexes.remove(indexes.iter().position(|&x| x == field_index).unwrap());
+                continue 'outer;
+            }
+        }
+        panic!("could not assign field!");
+    }
+    assigned
+}
+
 #[aoc(day16, part2)]
-pub fn part2(input: &Input) -> i32 {
-    todo!()
+pub fn part2(input: &Input) -> i64 {
+    let valid_tickets = input
+        .nearby_tickets
+        .iter()
+        .filter(|ticket| {
+            ticket
+                .iter()
+                .all(|&value| input.fields.iter().any(|field| field.is_valid(value)))
+        })
+        .cloned()
+        .collect::<Vec<_>>();
+
+    let assignments = assign_fields(&input.fields, &valid_tickets);
+    assignments
+        .into_iter()
+        .filter(|(field, _)| field.name.starts_with("departure"))
+        .map(|(_, index)| input.my_ticket[index] as i64)
+        .product()
 }
